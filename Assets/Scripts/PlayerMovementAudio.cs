6807b969cf505dda5c3b1d90c959f12f042d3968
Reza Saker <saker.reza001@hotmail.com>
2022-12-23 04:19:08 -0600

    Added audio clips for player movement

--- Assets/Scripts/PlayerMovementAudio.cs
+++ Assets/Scripts/PlayerMovementAudio.cs
@@ -8,8 +8,47 @@ public class PlayerMovementAudio : MonoBehaviour
 
     [SerializeField]
     private AudioClip[] footstepClip;
+
+    private CharacterController characterController;
+
+    [HideInInspector]
+    public float volMin, volMax;
+
+    private float accumulatedDistance;
+
+    [HideInInspector]
+    public float stepDistance;
+
+    void Awake()
+    {
+        footstepSound = GetComponent<AudioSource>();
+        characterController = GetComponentInParent<CharacterController>();
+    }
+
     void Update()
     {
-        
+        PlaySound();
+    }
+
+    void PlaySound() 
+    {
+        if(!characterController.isGrounded)
+        {
+            return;
+        }
+
+        if(characterController.velocity.sqrMagnitude > 0)
+        {
+            accumulatedDistance += Time.deltaTime;
+
+            if(accumulatedDistance > stepDistance)
+            {
+                footstepSound.volume = Random.Range(volMin, volMax);
+                footstepSound.clip = footstepClip[Random.Range(0, footstepClip.Length)];
+                footstepSound.Play();
+
+                accumulatedDistance = 0f;
+            }
+        }
     }
 }
--- Assets/Scripts/PlayerMovementModes.cs
+++ Assets/Scripts/PlayerMovementModes.cs
@@ -14,12 +14,31 @@ public class PlayerMovementModes : MonoBehaviour
     private bool isCrouching;
 
     private PlayerMovement playerMovement;
+    private PlayerMovementAudio playerMovementAudio;
     private Transform lookRoot;
+
+    private float runVol = 1f;
+    private float crouchVol = 0.1f;
+    private float walkVolMin = 0.2f;
+    private float walkVolMax = 0.6f;
+
+    private float walkStepDistance = 0.4f;
+    private float runStepDistance = 0.25f;
+    private float crouchStepDistance = 0.5f;
 
     void Awake()
     {
         playerMovement = GetComponent<PlayerMovement>();
         lookRoot = transform.GetChild(0);
+
+        playerMovementAudio = GetComponentInChildren<PlayerMovementAudio>();
+    }
+
+    void Start()
+    {
+        playerMovementAudio.volMin = walkVolMin;
+        playerMovementAudio.volMax = walkVolMax;
+        playerMovementAudio.stepDistance = walkStepDistance;
     }
 
     void Update()
@@ -32,11 +51,17 @@ public class PlayerMovementModes : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.LeftShift) && !isCrouching) 
         {
             playerMovement.speed = runSpeed;
+            playerMovementAudio.stepDistance = runStepDistance;
+            playerMovementAudio.volMin = runVol;
+            playerMovementAudio.volMax = runVol;
         }
 
         if(Input.GetKeyUp(KeyCode.LeftShift) && !isCrouching)
         {
             playerMovement.speed = walkSpeed;
+            playerMovementAudio.volMin = walkVolMin;
+            playerMovementAudio.volMax = walkVolMax;
+            playerMovementAudio.stepDistance = walkStepDistance;
         }
 
         if(Input.GetKeyDown(KeyCode.C))
@@ -45,11 +70,20 @@ public class PlayerMovementModes : MonoBehaviour
             {
                 lookRoot.localPosition = new Vector3(0f, normalHeight, 0f);
                 playerMovement.speed = walkSpeed;
+
+                playerMovementAudio.volMin = walkVolMin;
+                playerMovementAudio.volMax = walkVolMax;
+                playerMovementAudio.stepDistance = walkStepDistance;
             }
             else 
             {
                 lookRoot.localPosition = new Vector3(0f, crouchHeight, 0f);
                 playerMovement.speed = crouchSpeed;
+
+                playerMovementAudio.stepDistance = crouchStepDistance;
+                playerMovementAudio.volMin = crouchVol;
+                playerMovementAudio.volMax = crouchVol;
+
             }
             isCrouching = !isCrouching;
         }
