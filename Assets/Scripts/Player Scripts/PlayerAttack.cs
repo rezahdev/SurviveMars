8902d27112c87178f0e9f60764719d2eab381f33
Reza Saker <saker.reza001@hotmail.com>
2022-12-24 01:45:03 -0600

    Added weapon fire functionality

--- Assets/Scripts/Player Scripts/PlayerAttack.cs
+++ Assets/Scripts/Player Scripts/PlayerAttack.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    private WeaponManager weaponManager;
+
+    public float fireRate = 15f;
+    public float damage = 20f;
+    private float nextTimeToFire;
+
+    void Awake()
+    {
+        weaponManager = GetComponent<WeaponManager>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +24,36 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Shoot();
+    }
+
+    void Shoot()
+    {
         
+        if(weaponManager.GetCurrentWeapon().fireType == WeaponFireType.MULTIPLE)
+        {
+            if(Input.GetMouseButton(0) && Time.time > nextTimeToFire) 
+            {
+                nextTimeToFire = Time.time + 1f / fireRate;
+                weaponManager.GetCurrentWeapon().Shoot();
+                // BulletFired()
+            }
+        }
+        else
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                if(weaponManager.GetCurrentWeapon().tag == Tags.AXE_TAG)
+                {
+                    weaponManager.GetCurrentWeapon().Shoot();
+                }
+
+                if(weaponManager.GetCurrentWeapon().bulletType == WeaponBulletType.BULLET)
+                {
+                    weaponManager.GetCurrentWeapon().Shoot();
+                    // BulletFired()
+                }
+            }
+        }
     }
 }
